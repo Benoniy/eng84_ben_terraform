b54c70b41b3e055c5b05ed1ab6c4e061f74c25f1
Ben <benranson5@gmail.com>
2021-05-10 17:23:08 +0100

    did a ton of stuff

--- main.tf
+++ main.tf
@@ -8,18 +8,55 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-
 # Creating a vpc
-
 resource "aws_vpc" "eng84_ben_terraform_vpc"{
     cidr_block = "13.37.0.0/16"
-    instance_tenacity = "default"
 
     tags = {
         Name = "eng_84_ben_terraform_vpc"
     }
 }
 
+
+# Create an internet gateway that attaches to our vpc
+resource "aws_internet_gateway" "eng84_ben_terraform_igw" {
+  vpc_id = aws_vpc.eng84_ben_terraform_vpc.id
+
+  tags = {
+    Name = "eng84_ben_terraform_igw"
+  }
+}
+
+# Edit the main route table
+resource "aws_default_route_table" "eng84_ben_terraform_rt" {
+  default_route_table_id = aws_vpc.eng84_ben_terraform_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eng84_ben_terraform_igw.id
+  }
+
+  tags = {
+    Name = "eng84_ben_terraform_rt"
+  }
+}
+
+# Creating a subnet for out vpc
+resource "aws_subnet" "eng84_ben_terraform_public_subnet"{
+    vpc_id = aws_vpc.eng84_ben_terraform_vpc.id
+    cidr_block = "13.37.1.0/24"
+
+
+    tags = {
+        Name = "eng84_ben_terraform_public_subnet"
+    }
+}
+
+# Associate route table with subnet
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.eng84_ben_terraform_public_subnet.id
+  route_table_id = aws_route_table.eng84_ben_terraform_rt.id
+}
 
 # Launching an EC2 using our app ami
 # The resource keyword is used to create instances
@@ -30,6 +67,7 @@ resource "aws_instance" "terraform_app" {
     instance_type = "t2.micro"
     associate_public_ip_address = true
     key_name = "eng84devops"
+    subnet_id = aws_subnet.eng84_ben_terraform_public_subnet.id
 
     tags = {
         Name = "eng84_ben_terraform_app"
